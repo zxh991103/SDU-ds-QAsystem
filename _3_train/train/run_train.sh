a453e6f4d89ce5259757e1c710c0fa1a146dad8d
zhaoxiaohui.1 <zhaoxiaohui.1@bytedance.com>
2021-02-26 11:12:45 +0800

    train file 20210226

--- _3_train/train/run_train.sh
+++ _3_train/train/run_train.sh
@@ -1,6 +1,6 @@
 CONFIG_FILE="train_config.json"
-SAVE_PATH="../../_4_model/models"
-
+mkdir "/Users/admin/Desktop/qaresult/model/albert_base_/textcnn_idcnn"
+SAVE_PATH="/Users/admin/Desktop/qaresult/model/albert_base_/textcnn_idcnn"
 python run_train.py \
     -config ${CONFIG_FILE} \
     -save_path ${SAVE_PATH} \
