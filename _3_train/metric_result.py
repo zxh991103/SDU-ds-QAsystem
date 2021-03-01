5897691daf779056fc0a67f877e1b779495bfea9
zhaoxiaohui.1 <zhaoxiaohui.1@bytedance.com>
2021-03-01 13:21:42 +0800

    train file 20210301

---
+++ _3_train/metric_result.py
@@ -0,0 +1,14 @@
+res = {'chinese_wwm_ext_L-12_H-768_A-12_/textcnn_idcnn': {'resclf': 0.8626760563380281, 'resner': 0.9844064386317919},
+       'chinese_wwm_ext_L-12_H-768_A-12_/dense_idcnn': {'resclf': 0.528169014084507, 'resner': 0.9759808853118729},
+       'chinese_wwm_ext_L-12_H-768_A-12_/textcnn_bilstm': {'resclf': 0.3855633802816901, 'resner': 0.8690895372233379},
+       'chinese_wwm_ext_L-12_H-768_A-12_/dense_bilstm': {'resclf': 0.8080985915492958, 'resner': 0.905558350100604},
+       'albert_base_/textcnn_idcnn': {'resclf': 0.8098591549295775, 'resner': 0.9823943661971845},
+       'albert_base_/dense_idcnn': {'resclf': 0.8380281690140845, 'resner': 0.9810110663983914},
+       'albert_base_/textcnn_bilstm': {'resclf': 0.8380281690140845, 'resner': 0.9742203219315906},
+       'albert_base_/dense_bilstm': {'resclf': 0.36795774647887325, 'resner': 0.8206740442655921},
+       'chinese_L-12_H-768_A-12_/textcnn_idcnn': {'resclf': 0.2992957746478873, 'resner': 0.7838279678068406},
+       'chinese_L-12_H-768_A-12_/dense_idcnn': {'resclf': 0.3415492957746479, 'resner': 0.7834507042253517},
+       'chinese_L-12_H-768_A-12_/textcnn_bilstm': {'resclf': 0.8503521126760564, 'resner': 0.9801307847082512},
+       'chinese_L-12_H-768_A-12_/dense_bilstm': {'resclf': 0.8327464788732394, 'resner': 0.9611418511066403}}
+
+
